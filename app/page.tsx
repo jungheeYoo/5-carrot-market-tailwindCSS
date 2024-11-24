9d7402928c37a4975fe8a966f4f08362a174b2d4
Joy <jungheeyoo.joy@gmail.com>
2024-11-24 19:44:58 +0900

    3-11. Directives, apply, layer

--- app/page.tsx
+++ app/page.tsx
@@ -273,28 +273,43 @@
 //   );
 // }
 
-//////////////////////////////////////////////////
-// ✅ 3-10. JIT - just in time
-// 🔶 Tailwind CSS 작동 방식
-// just in time compiler
-// Tailwind CSS 파일이 아니라 compiler 이다
-// 우리가 파일을 저장할 때마다 Tailwind 는 파일을 스캔해서 class name들을 추출하고
-// 추출한 것을 CSS 코드로 변환한다. 그래서 class name을 생성할 수 있는 것이다
-// 미리 생성된 class name 파일은 존재하지 않음!
-// compiler 가 나의 class name 을 읽어들여서 class name 들로부터 추출해낸 CSS code를 생성할 것이다
+// //////////////////////////////////////////////////
+// // ✅ 3-10. JIT - just in time
+// // 🔶 Tailwind CSS 작동 방식
+// // just in time compiler
+// // Tailwind CSS 파일이 아니라 compiler 이다
+// // 우리가 파일을 저장할 때마다 Tailwind 는 파일을 스캔해서 class name들을 추출하고
+// // 추출한 것을 CSS 코드로 변환한다. 그래서 class name을 생성할 수 있는 것이다
+// // 미리 생성된 class name 파일은 존재하지 않음!
+// // compiler 가 나의 class name 을 읽어들여서 class name 들로부터 추출해낸 CSS code를 생성할 것이다
 
-// 🔶 arbitrary value(대체 값)
-// just in time compiler 덕분에 arbitrary value(대체 값) 을 가질 수 있다
-// 원하는 스타일을 찾을 수 없거나, 사이즈 등을 찾을 수 없을 때 [] 대괄호 안헤 필요한 것을 넣음
-// theme 확장 가능
+// // 🔶 arbitrary value(대체 값)
+// // just in time compiler 덕분에 arbitrary value(대체 값) 을 가질 수 있다
+// // 원하는 스타일을 찾을 수 없거나, 사이즈 등을 찾을 수 없을 때 [] 대괄호 안헤 필요한 것을 넣음
+// // theme 확장 가능
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+//         <button className="w-full bg-black h-10 text-white rounded-sexy-name me-tomato">
+//           Submit
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
+//////////////////////////////////////////////////
+// ✅ 3-11. Directives
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+    <main className="bg-red-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-        <button className="w-full bg-black h-10 text-white rounded-sexy-name me-tomato">
-          Submit
-        </button>
+        <input />
+        <a href="#">aaaa</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
