4f664b721c97466e082313e18a54f769cd4df408
Joy <jungheeyoo.joy@gmail.com>
2024-11-24 17:41:31 +0900

    3-9. Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -169,57 +169,97 @@
 //   );
 // }
 
+// //////////////////////////////////////////////////
+// // ✅ 3-7. State Modifiers
+// // 자식 선택자
+// // *:
+// // has
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div
+//         className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
+//       has-[:invalid]:ring-red-100"
+//       >
+//         <input
+//           className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+//           type="text"
+//           required
+//           placeholder="Email address"
+//         />
+//         <span className="text-red-500 font-medium hidden peer-invalid:block ">
+//           Email is required.
+//         </span>
+//         <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
+//           Log in
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
+// //////////////////////////////////////////////////
+// // ✅ 3-8. Lists and Animations
+// // Modifiers
+// // odd: , even: 홀짝 스타일 주기
+// // first, last
+// // empty state : 만약에 비어있다면 무엇을 한다 skeleton 을 만들 수 있음
+
+// // animate
+// // animate-bounce
+// // animate-spin
+// // animate-pulse : skeleton : loaing state 같은 것을 의미, text 가 도착하기 전의 UI의 모습을 보여줌
+// // animate-ping : notifications 같은 것에 사용
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
+//         {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
+//           <div key={index} className="flex items-center gap-5 ">
+//             <div className="size-10 bg-blue-400 rounded-full " />
+//             <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+//               {person}
+//             </span>
+//             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+//               <span className="z-10">{index}</span>
+//               <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// }
+
 //////////////////////////////////////////////////
-// ✅ 3-7. State Modifiers
-// 자식 선택자
-// *:
-// has
+// ✅ 3-9. Group Modifiers
+// 부모 state에 따라 자식 스타일을 수정
+// group-hover:
+// group-focus-within:
+// 그룹이 내부에 포커스 된 input을 갖고 있을 때만 true가 됨
 
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div
-        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
-      has-[:invalid]:ring-red-100"
-      >
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block ">
-          Email is required.
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
-          Log in
-        </button>
-      </div>
-    </main>
-  );
-}
-
-//////////////////////////////////////////////////
-// ✅ 3-8. Lists and Animations
-// Modifiers
-// odd: , even: 홀짝 스타일 주기
-// first, last
-// empty state : 만약에 비어있다면 무엇을 한다 skeleton 을 만들 수 있음
-
-// animate
-// animate-bounce
-// animate-spin
-// animate-pulse : skeleton : loaing state 같은 것을 의미, text 가 도착하기 전의 UI의 모습을 보여줌
-// animate-ping : notifications 같은 것에 사용
-
-export default function Home() {
-  return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
-        {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 ">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {/* ✨ group-focus-within: */}
+        <div className="group flex flex-col">
+          <input
+            className="bg-gray-100 w-full"
+            placeholder="Write your email"
+          />
+          <span className="group-focus-within:block hidden">
+            Make sure it tis a valid email...
+          </span>
+          <button>Submit</button>
+        </div>
+        {/* ✨ group-hover: */}
+        {/* {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
+          <div key={index} className="flex items-center group gap-5 ">
             <div className="size-10 bg-blue-400 rounded-full " />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
               {person}
             </span>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
@@ -227,7 +267,7 @@ export default function Home() {
               <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </main>
   );
