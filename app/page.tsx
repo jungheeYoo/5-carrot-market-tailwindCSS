880b82ce09e92ad94a17be5ea42a0b27eb0f795d
Joy <jungheeyoo.joy@gmail.com>
2024-11-24 16:48:03 +0900

    3.7 State Modifiers 3-8. Lists and Animations, Modifiers, animate

--- app/page.tsx
+++ app/page.tsx
@@ -142,28 +142,92 @@
 //   );
 // }
 
+// //////////////////////////////////////////////////
+// // ✅ 3-6. Form Modifiers
+// // gradient
+// // invalid
+// // peer - 형제 같은 것
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
+//       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+//         <input
+//           className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+//           type="email"
+//           required
+//           placeholder="Email address"
+//         />
+//         <span className="text-red-500 font-medium hidden peer-invalid:block">
+//           Email is required.
+//         </span>
+//         <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-required:bg-green-500">
+//           Log in
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
+// //////////////////////////////////////////////////
+// // ✅ 3-7. State Modifiers
+// // 자식 선택자
+// // *:
+// // has
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div
+//         className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
+//       has-[:invalid]:ring-red-100"
+//       >
+//         <input
+//           className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+//           type="text"
+//           required
+//           placeholder="Email address"
+//         />
+//         <span className="text-red-500 font-medium hidden peer-invalid:block ">
+//           Email is required.
+//         </span>
+//         <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
+//           Log in
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
 //////////////////////////////////////////////////
-// ✅ 3-6. Form Modifiers
-// gradient
-// invalid
-// peer - 형제 같은 것
+// ✅ 3-8. Lists and Animations
+// Modifiers
+// odd: , even: 홀짝 스타일 주기
+// first, last
+// empty state : 만약에 비어있다면 무엇을 한다 skeleton 을 만들 수 있음
+
+// animate
+// animate-bounce
+// animate-spin
+// animate-pulse : skeleton : loaing state 같은 것을 의미, text 가 도착하기 전의 UI의 모습을 보여줌
+// animate-ping : notifications 같은 것에 사용
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
-        <input
-          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required.
-        </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-required:bg-green-500">
-          Log in
-        </button>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
+        {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 ">
+            <div className="size-10 bg-blue-400 rounded-full " />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
